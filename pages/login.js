e86bea2b3f0306dfd929d0094cd226926b2281b8
Monwar <monwar259@gmail.com>
2021-12-04 18:31:07 +0000

    Fixed styling on no auth screens

--- pages/login.js
+++ pages/login.js
@@ -16,9 +16,9 @@ export default function login() {
 
   return (
     <Layout isAuth={false}>
-      <div className="h-screen grid place-items-center">
+      <div className="min-h-screen grid place-items-center">
         <img
-          className="absolute z-0 brightness-50 h-full w-full object-cover top-0 left-0"
+          className="fixed z-0 brightness-50 h-full w-full object-cover top-0 left-0"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/23c72870-12c8-4682-9f55-337a083ccfa7/a51f79cc-b884-4b64-a5a9-75e04de2c383/GB-en-20211129-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt=""
         />
